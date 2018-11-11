904bbeb950f66ff929dc45456ac978a4b7edcc20
Jhonatas <costajhonatas9@gmail.com>
2018-11-10 22:39:54 -0200

    Update and rename main.c to Main.c

--- Main.c
+++ Main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include "Funcoes.h"
-int main ()
+
+void main(void)
 {
   printf("O programa converte números decimais inteiros para outra base.\nO programa recebe um número em decimal e a base alvo.\nInicialmente, caso a base não seja informada será usada a base 2.\nSe uma base diferente for informada ela passará a ser a base padrão.\nExemplos de uso:\n\t>> Digite um número e a base (opcional): <decimal> <base>\n\t>> Digite um número e a base (opcional): 25 Irá converter o decimal 25 para binário (base 2, padrão)\n\t>> Digite um número e a base (opcional): 40 5 Irá converter o decimal 40 para base 5\n\t>> Digite um número e a base (opcional): 70 Irá converter o decimal 70 para base 5 (última base informada).\nExecutar o programa passando -ajuda, mostra essa mensagem.\nExcultar o programa passando -info, mostra informações dos desenvolvedores.\nExecutar o programa sem parâmetros encerra o programa.\n\n");
   compara();
-  return 0;
 }
