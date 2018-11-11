3f79f57481535f213c7a46919b279da430533d0e
Jhonatas <costajhonatas9@gmail.com>
2018-11-11 12:12:07 -0200

    Update Funcoes.h

--- Funcoes.h
+++ Funcoes.h
@@ -1,3 +1,12 @@
+/* 
+  Laurentino, J.I.C.
+  Laboratório de Inteligência Computacional Aplicada a Negócios (LABICAN) 
+  Departamento de Computação e Tecnologia (DCT) 
+  Bacharelando em Sistemas de Informação (BSI)
+  Universidade Federal do Rio Grande do Norte – Caicó – RN – Brasil
+  
+  11/11/2018
+*/
 #ifndef Funcoes_H   
 #define Funcoes_H
 int compara();
